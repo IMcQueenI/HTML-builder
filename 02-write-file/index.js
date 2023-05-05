b56406d49e08cdc1a62f797c5daaf32cb11d470d
McQueen <m.gorelenko@mail.ru>
2023-05-05 20:33:40 +0300

    feat: add solution for task #2

---
+++ 02-write-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout,} = process;
+
+const mqPath = path.join(__dirname, 'data.txt');
+const mqOut = fs.createWriteStream(mqPath);
+
+stdout.write('Hello, i am McQueen, write me something! \n');
+
+stdin.on('data', data => {
+	if (data.toString().trim() === 'exit') {
+		process.exit();
+	} else {
+		mqOut.write(data);
+	};
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => stdout.write('Bye, dear friend!'));
